284a46199625a0081140277a908a662d5c864369
solvencino <harshparamar84@gmail.com>
2022-02-14 17:27:10 +0530

    handle socket open and close

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 const dht = require('@hyperswarm/dht')
 const Speaker = require('speaker')
-const fs = require('fs')
+
+// Create the Speaker instance
+const speaker = new Speaker({
+  channels: 2, // 2 channels
+  bitDepth: 16, // 16-bit samples
+  sampleRate: 48000, // 48000 Hz sample rate
+})
 
 const node = new dht()
 
-const buf = Buffer.from('', 'hex')
+const buf = Buffer.from('f5a041429288fcf388f0301580b926f9729c11bc21f11c7b0c62d231a0667e1b', 'hex')
 
 const socket = node.connect(buf)
 
-socket.pipe(fs.createWriteStream('./new.pcm'))
+socket.on('open', () => {
+  socket.pipe(speaker)
+})
--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const dht = require('@hyperswarm/dht')
-
+const cp = require('child_process')
+const sockets = []
 const node = new dht()
+const proc = cp.spawn('sox', '-q -t pulseaudio alsa_output.pci-0000_00_1f.3.analog-stereo.monitor --buffer 512 -t raw -'.split(' '))
+
+proc.stdout.on('data', (chunk) => {
+  sockets.forEach((s) => {
+    s.write(chunk)
+  })
+})
 
 const server = node.createServer((socket) => {
-  process.stdin.pipe(socket)
+  socket.on('open', () => {
+    sockets.push(socket)
+  })
+  socket.on('error', () => removeSocket(socket))
+  socket.on('close', () => removeSocket(socket))
 })
+
+function removeSocket(socket) {
+  sockets.splice(
+    sockets.findIndex((t) => t === socket),
+    1
+  )
+}
 
 server.listen().then(() => {
   console.log('DHT Key:', server.address().publicKey.toString('hex'))
